15eb1e59c9af5b7cec2574c6cd34bdce5a3cde8d
Juho S <juho.salli@gmail.com>
2022-10-30 13:55:40 +0200

    add limits to the thingy spawn area to keep it inside the play area

--- src/thingy.rs
+++ src/thingy.rs
@@ -13,6 +13,11 @@ impl Plugin for ThingyPlugin {
 }
 
 pub fn spawn_thingy(mut commands: Commands) {
+    let y_upper = (WINDOW_HEIGHT / 2.) - 110.;
+    let y_lower = -(WINDOW_HEIGHT / 2.) + 60.;
+
+    let x_upper = -(WINDOW_WIDTH / 2.0) + 80.;
+    let x_lower = (WINDOW_WIDTH / 2.0) - 80.;
     let t = shapes::Rectangle {
         origin: RectangleOrigin::Center,
         extents: Vec2 { x: 30.0, y: 30.0 },
@@ -24,8 +29,8 @@ pub fn spawn_thingy(mut commands: Commands) {
             DrawMode::Fill(FillMode::color(Color::BLACK)),
             Transform {
                 translation: Vec3 {
-                    x: rng.gen_range(-(WINDOW_WIDTH / 2.0)..(WINDOW_WIDTH / 2.0)) as f32,
-                    y: rng.gen_range(-(WINDOW_HEIGHT / 2.0)..(WINDOW_HEIGHT / 2.0)) as f32,
+                    x: rng.gen_range(x_upper..x_lower) as f32,
+                    y: rng.gen_range(y_lower..y_upper) as f32,
                     z: 10.,
                 },
                 ..default()
